5032f82af06fb17160bff99570ff0fb881d0e24a
Ashish Patil <ashishpatil9329@gmail.com>
2023-04-24 12:14:30 +0530

    all files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name ="diamond-price-predn",
+      version ="0.0.1",
+      author ="Ashish",
+      author_email="ashishpatil9329@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+       )
